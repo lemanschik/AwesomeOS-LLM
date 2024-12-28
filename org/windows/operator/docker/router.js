25174ffd7936ae5a5e6462a9f7437d4040dba7ee
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2024-12-28 11:42:05 +0100

    Update router.js

--- org/windows/operator/docker/router.js
+++ org/windows/operator/docker/router.js
@@ -1,4 +1,5 @@
 /** Tested and Aligned with llama3.2:3b-instruct */
+/** Includes knowledge about wsl and other ways to run docker containers */
 export const orgLinuxInstallerRouter = { 
   messages: [],
   tools: {}, 
